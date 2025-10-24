5d2bb72f60c6470e44298dedca8890a91fbee75a
LeonardoButne <leonardobutne78@gmail.com>
2025-10-24 17:18:40 +0200

    fix: comment out unused SdgSection import in Home component

--- pensa-web/src/pages/public/home/Home.tsx
+++ pensa-web/src/pages/public/home/Home.tsx
@@ -7,7 +7,7 @@ import { InfoCards } from "../../../components/home/InfoCards/InfoCards";
 import { MentalHealth } from "../../../components/home/MentalHealth/MentalHealth";
 import { NewsSection } from "../../../components/home/NewsSection/NewsSection";
 import { PartnersSection } from "../../../components/home/PartnersSection/PartnersSection";
-import { SdgSection } from "../../../components/home/SdgSection/SdgSection";
+// import { SdgSection } from "../../../components/home/SdgSection/SdgSection";
 
 export function Home() {
   return (
